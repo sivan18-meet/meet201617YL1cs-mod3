9abb2a7d0f4fcdf28e62379bd6363ab6c4e31576
mahmoud17-meet <mahmoud17@meet.mit.edu>
2016-11-24 19:35:35 +0200

    added more lines to the code

--- UserAccount.py
+++ UserAccount.py
@@ -11,6 +11,8 @@ class UserAccount :
         :param secret: user's secret, accessible only with passworld (string)
         """
         self.username=username
+        self.password=password
+        self.secret=secret
         #Complete the __init__ method
 
     def print_password(self,password_attempt):
@@ -20,6 +22,12 @@ class UserAccount :
 
         :param password_attempt: guess for the password (string)
         """
+        
+        self.password_attempt=password_attempt
+        if self.password==self.password_attempt:
+            print(secret)
+        
+            
         #Complete this method - check the input variable, password_attempt,
         #against the password for this user (the value of the instance variable, password).
         #If, and only if, there's a match, then print the user's secret to the screen.
