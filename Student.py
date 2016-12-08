f2838a206e2bb8385cbe4387379f283dac08514a
sivan18-meet <sbmislovely@gmail.com>
2016-12-08 18:30:59 +0200

    made a class to describe a student

--- Student.py
+++ Student.py
@@ -7,3 +7,11 @@ class student:
         self.ice_cream=ice_cream
     def print_summary(self):
         print(self.name+" "+self.hometown+" "+self.age+" "+self.height+" "+self.ice_cream)
+
+    def get_giraffe_gap(self,get_giraffe_gap):
+        return self.get_giraffe_gap=500-self.height
+
+    
+        
+        
+        
